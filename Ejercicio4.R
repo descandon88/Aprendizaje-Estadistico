b9b2724b667cd3328b8beac190c30075ba6c5105
David Escandón <david.escandon.v@gmail.com>
2021-08-01 19:39:24 -0300

    Add files via upload

--- Ejercicio4.R
+++ Ejercicio4.R
@@ -2,19 +2,24 @@
 library(ISLR)
 library(rpart)
 library(randomForest)
-
+library(tree)
+## Librerias para graficas chulas
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
 library(ggplot2)
 library(Hmisc) ## para apliocar describe
 data("Carseats")
 
-#EstadÃ­stico del dataset
+#Estadístico del dataset
 di <- describe(Carseats)
 summary(Carseats)
+str(Carseats)
 
 ##1. Dividir los datos en un conjunto de entrenamiento y un conjunto de control.
 
 Carseats=data.frame(Carseats)
-# Defino un tamaÃ±o de la muestra
+# Defino un tamaño de la muestra
 crseats_size = floor(0.75*nrow(Carseats)) ## lo defino en un 75%
 
 set.seed(123)   # set seed to ensure you always have same random numbers generated
@@ -25,4 +30,11 @@ test=Carseats[-train_ind,]
 ##2. Ajustar un arbol de regresion utilizando la biblioteca rpart a los datos de entrenamiento y
 ##estimar el error de generalizacion del modelo.
 
+tree.carseats=tree(,-Sales ,train )
+
+mytree <- rpart( Sales ~ CompPrice + Income + Advertising
+                 + Population, data = train, method = "class")
+
+fancyRpartPlot(mytree, caption = NULL)
+rpart.plot(mytree)
 
